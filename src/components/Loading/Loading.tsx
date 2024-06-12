f05a1d1709d7903f62b0b53113558d650282ee15
hcustovic1 <custovicharis@gmail.com>
2024-06-12 21:42:58 +0200

    fix: Loading component made accessible

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -3,8 +3,8 @@ import styles from './Loading.module.css';
 
 export const Loading: React.FC = () => {
   return (
-    <div className={styles.loaderContainer}>
-      <div className={styles.spinner}></div>
+    <div className={styles.loaderContainer} role="alert" aria-busy="true">
+      <div className={styles.spinner} aria-hidden="true"></div>
       <p>Loading...</p>
     </div>
   );
